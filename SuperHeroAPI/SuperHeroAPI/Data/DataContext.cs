50b5f3bf99c4d11c996bc323bf81c17874bdbf21
Tetri Mesquita Neto <tetri.neto@aliare.co>
2022-11-22 22:35:34 -0300

    SuperHero angular CRUD + dotnet API

---
+++ SuperHeroAPI/SuperHeroAPI/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Bogus;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace SuperHeroAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<SuperHero> SuperHeroes => Set<SuperHero>();
+
+    }
+}
